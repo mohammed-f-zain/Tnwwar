63fff09303ed9a4c542d106f044423f74a087f7f
mohammed-f-zain <mohammed.f.zain@gmail.com>
2024-05-02 19:39:44 +0300

    setup complete

--- tnwwar/src/App.js
+++ tnwwar/src/App.js
@@ -1,24 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from './pages/Login';
+import Home from './pages/Home';
 
 function App() {
+  const [showSplash, setShowSplash] = useState(true);
+
+  useEffect(() => {
+    // Hide splash screen after 3 seconds
+    const timer = setTimeout(() => {
+      setShowSplash(false);
+    }, 2000);
+
+    // Cleanup timer on component unmount
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        {showSplash ? (
+          <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+            <h1>HIIIIII</h1>
+          </div>
+        ) : (
+          <Routes>
+
+              <Route path="/" element={<Home />} />
+              <Route path="/login" element={<Login />} />
+          </Routes>
+        )}
+      </div>
+    </Router>
   );
 }
 
---
+++ tnwwar/src/pages/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div>
+      Hello from Home
+      <a href='/login'>go to log in</a>
+    </div>
+  )
+}
+
+export default Home
