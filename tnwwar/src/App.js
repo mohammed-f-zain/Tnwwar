e119b250c6df2b9ba2411a5a32826f12ba097887
mohammed-f-zain <mohammed.f.zain@gmail.com>
2024-05-09 01:21:24 +0300

    Version 1 of home page, link signup and signin with api + validation, fetching all products, product details with no style

--- tnwwar/src/App.js
+++ tnwwar/src/App.js
@@ -1,37 +1,57 @@
 import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Login from './pages/Login';
 import Home from './pages/Home';
+import Login from './pages/Login';
+import SavedPage from './pages/SavedPage';
+import Cart from './pages/Cart';
+import PrchaseHistory from './pages/PrchaseHistory';
+import Profile from './pages/Profile';
+import ProductDetails from './pages/ProductDetails';
+import NotFound from './pages/NotFound';
+import { AuthProvider } from './context/AuthContext'; 
+import Logo from "./assets/logos/mainLogo.svg"
+import Signup from './pages/Signup';
 
 function App() {
   const [showSplash, setShowSplash] = useState(true);
 
   useEffect(() => {
-    // Hide splash screen after 3 seconds
     const timer = setTimeout(() => {
       setShowSplash(false);
     }, 2000);
-
-    // Cleanup timer on component unmount
     return () => clearTimeout(timer);
   }, []);
 
   return (
-    <Router>
-      <div>
+    <AuthProvider> 
+      <Router>
         {showSplash ? (
-          <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
-            <h1>HIIIIII</h1>
+          <div
+            style={{
+              display: 'flex',
+              justifyContent: 'center',
+              alignItems: 'center',
+              height: '100vh',
+              backgroundColor: '#f5f5f5',
+            }}
+          >
+            <img src={Logo} />
           </div>
         ) : (
           <Routes>
-
-              <Route path="/" element={<Home />} />
-              <Route path="/login" element={<Login />} />
+            <Route path="/" element={<Home />} /> 
+            <Route path="/login" element={<Login />} />
+            <Route path="/saved" element={<SavedPage />} /> 
+            <Route path="/cart" element={<Cart />} /> 
+            <Route path="/prchase-history" element={<PrchaseHistory />} /> 
+            <Route path="/profile" element={<Profile />} /> 
+            <Route path="/productDetails/:id" element={<ProductDetails />} />
+            <Route path="/signup" element={<Signup />} /> 
+            <Route path="*" element={<NotFound />} /> 
           </Routes>
         )}
-      </div>
-    </Router>
+      </Router>
+    </AuthProvider>
   );
 }
 
--- tnwwar/src/pages/Home.js
+++ tnwwar/src/pages/Home.js
@@ -1,12 +1,36 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import Aside from '../components/a-sideBar/Aside';
+import useTabNavigation from '../hooks/useTabNavigation';
+import Header from "../components/header/header"
+import ProductCarousel from '../components/Home/ProductCarousel';
+import Offertab from '../components/Home/Offertab';
+import RightAside from '../components/Right-aside/RightAside';
 
-function Home() {
+
+
+function HomeScreen() {
+  const { activeTab, handleTabChange } = useTabNavigation();
+  const [products, setProducts] = useState([]);
+
   return (
-    <div>
-      Hello from Home
-      <a href='/login'>go to log in</a>
+    <div className="container-fluid">
+      <div className="row">
+        <div className="col-sm-2">
+          <Aside activeTab={activeTab} handleTabChange={handleTabChange} />
+        </div>
+        <div className="col-sm-8">
+          <Header />
+          <h3 style={{ color: "#009393" }}>Explore Popular Categories</h3>
+          <ProductCarousel products={products} />
+          <h3 style={{ color: "#009393" }}>Hot Offer</h3>
+          <Offertab />
+        </div>
+        <div className="col-sm-2">
+          <RightAside />
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Home
+export default HomeScreen;
